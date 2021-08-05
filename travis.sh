b643e55605a9dc55d89506ec53ec0e1ad85bd64d
François De Keersmaeker <francois.dekeersmaeker@student.uclouvain.be>
2021-08-05 22:34:00 +0200

    Updated Travis script for warnings

--- travis.sh
+++ travis.sh
@@ -4,68 +4,85 @@
 # when a commit is pushed to the repository.
 # This script is run into the syllabus container.
 
+# Script return value: 0 if success, anything else if failed
+RETURN=0
+
+# If the file passed as argument indicates warnings,
+# switch the return value of the script to 1.
+#   :param $1: file to check for warnings
+check_warnings () {
+    if [[ $(grep WARNING $1) ]]; then
+        RETURN=1
+    fi
+}
+
 # Exit on error
-#set -e
+set -e
 
 
 #####################
 # CHECK COMPILATION #
 #####################
-echo -e "\n**** FULL COMPILATION - LATEXPDF, EPUB, HTML ****\n"
+echo -e "\n\n**** FULL COMPILATION - LATEXPDF, EPUB, HTML ****\n\n"
 make
 
 
 ##################
 # CHECK SPELLING #
 ##################
-echo -e "\n**** SPELLING CHECK ****\n"
+echo -e "\n\n**** SPELLING CHECK ****\n"
 
 # Sphinx flags used here, not in with `make` compilation:
 #  -n   Run in nit-picky mode. Currently, this generates warnings for all missing references.
-#  -W   Turn warnings into errors. This means that the build stops at the first warning and sphinx-build exits with exit status 1.
-#  -T   output full traceback
+#  -w   write warnings (and errors) to the given file, in addition to standard error.
 
 # Theorie
 cd Theorie
 echo -e "\n**** Theorie ****\n"
-sphinx-build -n -b spelling . /tmp
+sphinx-build -n -w log.txt -b spelling . /tmp
+check_warnings log.txt
 
 # Outils
 cd ../Outils
 echo -e "\n**** Outils ****\n"
-sphinx-build -n -b spelling . /tmp
+sphinx-build -n -w log.txt -b spelling . /tmp
+check_warnings log.txt
 
 # Exercices
 cd ../Exercices
 echo -e "\n**** Exercices ****\n"
-sphinx-build -n -b spelling . /tmp
+sphinx-build -n -w log.txt -b spelling . /tmp
+check_warnings log.txt
 
 
 ###############
 # CHECK LINKS #
 ###############
-echo -e "\n**** LINKS CHECK ****\n"
+echo -e "\n\n**** LINKS CHECK ****\n"
 
 # Sphinx flags used here, not in with `make` compilation:
 #  -n   Run in nit-picky mode. Currently, this generates warnings for all missing references.
-#  -W   Turn warnings into errors. This means that the build stops at the first warning and sphinx-build exits with exit status 1.
-#  -T   output full traceback
+#  -w   write warnings (and errors) to the given file, in addition to standard error.
 
 # Theorie
 cd ../Theorie
 echo -e "\n**** Theorie ****\n"
-sphinx-build -n -b linkcheck . /tmp
+sphinx-build -n -w log.txt -b linkcheck . /tmp
+check_warnings log.txt
 
 # Outils
 cd ../Outils
 echo -e "\n**** Outils ****\n"
-sphinx-build -n -b linkcheck . /tmp
+sphinx-build -n -w log.txt -b linkcheck . /tmp
+check_warnings log.txt
 
 # Exercices
 cd ../Exercices
 echo -e "\n**** Exercices ****\n"
-sphinx-build -n -b linkcheck . /tmp
+sphinx-build -n -w log.txt -b linkcheck . /tmp
+check_warnings log.txt
 
 
 # End of script
 cd ../..
+exit $RETURN
